1a0786a1fa63f29d519c64440ec37e9dbdf1da69
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2024-10-17 21:45:51 +0100

    Add ids to headings

--- mdsycx/src/parser.rs
+++ mdsycx/src/parser.rs
@@ -1,5 +1,7 @@
 //! Parse MD with custom extensions.
 
+use std::collections::HashMap;
+
 use pulldown_cmark::html::push_html;
 use pulldown_cmark::Options;
 use quick_xml::events::Event as XmlEvent;
@@ -27,8 +29,22 @@ pub enum ParseError {
 pub struct ParseRes<T = ()> {
     /// The parsed MD front matter. If no front matter was present, this has a value of `None`.
     pub front_matter: T,
+    /// An outline of the document. Contains the text and the ids of all the headings found in the
+    /// document.
+    pub headings: Vec<OutlineHeading>,
     /// The parsed file. This should be passed when rendering the Markdown with Sycamore.
     pub body: BodyRes,
+}
+
+/// A heading in the document.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct OutlineHeading {
+    /// The anchor associated with the heading.
+    pub id: String,
+    /// The text of the heading.
+    pub text: String,
+    /// The level of the heading.
+    pub level: u32,
 }
 
 /// The parsed markdown file.
@@ -62,9 +78,11 @@ where
         if let Some((front_matter_str, body_str)) = rest.split_once("---") {
             let front_matter = serde_yaml::from_str(front_matter_str)?;
 
+            let (headings, body) = parse_md(body_str);
             Ok(ParseRes {
                 front_matter,
-                body: parse_md(body_str),
+                headings,
+                body,
             })
         } else {
             Err(ParseError::MissingFrontMatterEndDelimiter)
@@ -72,39 +90,75 @@ where
     } else {
         // Try to parse front matter from an empty string.
         let front_matter = serde_yaml::from_str::<T>("")?;
+        let (headings, body) = parse_md(input);
         Ok(ParseRes {
             front_matter,
-            body: parse_md(input),
+            headings,
+            body,
         })
     }
 }
 
 /// Parse Markdown into structured events.
-fn parse_md(input: &str) -> BodyRes {
+fn parse_md(input: &str) -> (Vec<OutlineHeading>, BodyRes) {
     let md_parser = pulldown_cmark::Parser::new_ext(input, Options::all()).peekable();
     let mut html = String::new();
     push_html(&mut html, md_parser);
 
+    let mut headings = Vec::new();
     let mut events = Vec::new();
-    parse_html(&html, &mut events);
+    parse_html(&html, &mut headings, &mut events);
 
-    BodyRes { events }
+    (headings, BodyRes { events })
 }
 
-fn parse_html(input: &str, events: &mut Vec<Event>) {
+#[derive(Debug, Default)]
+struct SlugState {
+    ids: HashMap<String, u32>,
+}
+
+impl SlugState {
+    pub fn slugify(&mut self, text: &str) -> String {
+        let slug = text
+            .to_lowercase()
+            .replace(|c: char| !c.is_ascii_alphanumeric(), "-")
+            .trim_matches('-')
+            .to_string();
+
+        let count = self.ids.entry(slug.clone()).or_insert(0);
+        *count += 1;
+
+        if *count > 1 {
+            format!("{}-{}", slug, count)
+        } else {
+            slug
+        }
+    }
+}
+
+fn parse_html(input: &str, headings: &mut Vec<OutlineHeading>, events: &mut Vec<Event>) {
     let mut reader = Reader::from_str(input);
 
     // Keep track of the element depth. If the depth is not 0 when parsing is finished, that means
     // that the HTML was malformed and we need to emit extra End tags.
     let mut depth = 0;
 
+    let mut slugger = SlugState::default();
+    let mut heading_title = None;
+
     let mut buf = Vec::new();
     loop {
         match reader.read_event_into(&mut buf) {
             Ok(XmlEvent::Start(start)) => {
-                events.push(Event::Start(
-                    String::from_utf8(start.name().0.to_vec()).unwrap(),
-                ));
+                let tag = start.name().0;
+                // Check if this is the start of a heading. If so, initialize `heading_title`.
+                if tag.len() == 2 && tag[0] == b'h' && tag[1].is_ascii_digit() {
+                    heading_title = Some(String::new());
+                }
+
+                let tag = String::from_utf8(tag.to_vec()).unwrap();
+
+                events.push(Event::Start(tag));
                 for attr in start.html_attributes().with_checks(false).flatten() {
                     events.push(Event::Attr(
                         String::from_utf8(attr.key.0.to_vec()).unwrap(),
@@ -113,7 +167,21 @@ fn parse_html(input: &str, events: &mut Vec<Event>) {
                 }
                 depth += 1;
             }
-            Ok(XmlEvent::End(_)) => {
+            Ok(XmlEvent::End(tag)) => {
+                // Check if this is the end of a heading. If so, set `heading_title` back to `None`
+                // and slug the title.
+                if tag.len() == 2 && tag[0] == b'h' && tag[1].is_ascii_digit() {
+                    if let Some(title) = heading_title.take() {
+                        let id = slugger.slugify(&title);
+                        events.push(Event::Attr("id".to_string(), id.clone()));
+                        headings.push(OutlineHeading {
+                            id,
+                            text: title,
+                            level: (tag[1] - b'0') as u32,
+                        });
+                    }
+                }
+
                 if depth != 0 {
                     events.push(Event::End);
                     depth -= 1;
@@ -122,9 +190,8 @@ fn parse_html(input: &str, events: &mut Vec<Event>) {
                 }
             }
             Ok(XmlEvent::Empty(start)) => {
-                events.push(Event::Start(
-                    String::from_utf8(start.name().0.to_vec()).unwrap(),
-                ));
+                let tag = String::from_utf8(start.name().0.to_vec()).unwrap();
+                events.push(Event::Start(tag));
                 for attr in start.html_attributes().with_checks(false).flatten() {
                     events.push(Event::Attr(
                         String::from_utf8(attr.key.0.to_vec()).unwrap(),
@@ -134,7 +201,11 @@ fn parse_html(input: &str, events: &mut Vec<Event>) {
                 events.push(Event::End);
             }
             Ok(XmlEvent::Text(text)) => {
-                events.push(Event::Text(text.unescape().unwrap().to_string()))
+                let text = text.unescape().unwrap().to_string();
+                if let Some(title) = heading_title.as_mut() {
+                    title.push_str(&text);
+                }
+                events.push(Event::Text(text))
             }
             Ok(XmlEvent::Eof) => break,
             Err(e) => {
@@ -162,8 +233,8 @@ mod tests {
     use super::*;
 
     fn check(input: &str, expect: Expect) {
-        let actual = parse_md(input);
-        expect.assert_eq(&format!("{:?}", actual.events));
+        let (_headings, body) = parse_md(input);
+        expect.assert_eq(&format!("{:?}", body.events));
     }
 
     #[test]
@@ -191,7 +262,7 @@ My text
 
 - List item"#,
             expect![[
-                r#"[Start("h1"), Text("My heading"), End, Text("\n"), Start("h2"), Text("My subtitle"), End, Text("\n"), Start("p"), Text("My text"), End, Text("\n"), Start("ul"), Text("\n"), Start("li"), Text("List item"), End, Text("\n"), End, Text("\n")]"#
+                r#"[Start("h1"), Text("My heading"), Attr("id", "my-heading"), End, Text("\n"), Start("h2"), Text("My subtitle"), Attr("id", "my-subtitle"), End, Text("\n"), Start("p"), Text("My text"), End, Text("\n"), Start("ul"), Text("\n"), Start("li"), Text("List item"), End, Text("\n"), End, Text("\n")]"#
             ]],
         );
     }
@@ -255,5 +326,29 @@ My text
                 r#"[Start("p"), Text("Some inline "), Start("span"), Start("i"), Text("text"), End, End, End, Text("\n")]"#
             ]],
         );
+    }
+
+    #[test]
+    fn check_slug() {
+        let mut state = SlugState::default();
+        assert_eq!(state.slugify("Hello World"), "hello-world");
+        assert_eq!(state.slugify("Hello World"), "hello-world-2");
+        assert_eq!(state.slugify("hello world"), "hello-world-3");
+        assert_eq!(state.slugify("hello-world"), "hello-world-4");
+        assert_eq!(state.slugify("hello world!"), "hello-world-5");
+
+        assert_eq!(state.slugify("Goodbye World"), "goodbye-world");
+    }
+
+    #[test]
+    fn check_duplicated_heading() {
+        check(
+            r#"
+# Hello World
+## Hello World!"#,
+            expect![[
+                r#"[Start("h1"), Text("Hello World"), Attr("id", "hello-world"), End, Text("\n"), Start("h2"), Text("Hello World!"), Attr("id", "hello-world-2"), End, Text("\n")]"#
+            ]],
+        )
     }
 }
